5df2c51f40d009b86c4c7863860e5f8025185023
akhil-stackcue <akhil.stackcue@gmail.com>
2025-08-05 17:05:27 +0530

    ledger bug fixed implemented collections
    ledger bug fixed implemented collections

--- Modules/Accounting/app/Http/Controllers/Ledger/LedgerController.php
+++ Modules/Accounting/app/Http/Controllers/Ledger/LedgerController.php
@@ -25,63 +25,49 @@ class LedgerController extends Controller
         return view('accounting::ledger.ledger');
     }
 
-    /**
-     * Get Ledger Entries for DataTable AJAX with server-side processing.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function entriesList(Request $request)
-    {
-        $query = Ledger::with([
-            'journalEntry.chartOfAccount.subcategory',
-            'journalEntry.chartOfAccount.mainCategory'
-        ]);
 
-        $searchValue = $request->input('search.value');
 
-        if (!empty($searchValue)) {
-            $query->whereHas('journalEntry.chartOfAccount', function ($q) use ($searchValue) {
-                $q->where('account_name', 'like', "%{$searchValue}%")
-                  ->orWhereHas('subcategory', function ($subQ) use ($searchValue) {
-                      $subQ->where('name', 'like', "%{$searchValue}%");
-                  })
-                  ->orWhereHas('mainCategory', function ($mainQ) use ($searchValue) {
-                      $mainQ->where('name', 'like', "%{$searchValue}%");
-                  });
-            });
-        }
+public function entriesList(Request $request)
+{
+    $query = ChartOfAccount::with(['subcategory', 'mainCategory']);
 
-        $totalRecords = Ledger::count();
-        $filteredRecords = $query->count();
-
-        $ledgers = $query
-            ->offset($request->input('start'))
-            ->limit($request->input('length'))
-            ->get();
-
-        $data = [];
-        foreach ($ledgers as $ledger) {
-            $journalEntry = $ledger->journalEntry;
-            $chartOfAccount = $journalEntry?->chartOfAccount;
-
-            $id = $chartOfAccount?->id ?? '-';
-
-            $data[] = [
-                'id'            => $id,
-                'account_name'  => $chartOfAccount?->account_name ?? '-',
-                'sub_category'  => $chartOfAccount?->subcategory?->name ?? '-',
-                'main_category' => $chartOfAccount?->mainCategory?->name ?? '-',
-            ];
-        }
-
-        return response()->json([
-            'draw' => intval($request->input('draw')),
-            'recordsTotal' => $totalRecords,
-            'recordsFiltered' => $filteredRecords,
-            'data' => $data
-        ]);
+    // Search filter
+    $searchValue = $request->input('search.value');
+    if (!empty($searchValue)) {
+        $query->where('account_name', 'like', "%{$searchValue}%")
+              ->orWhereHas('subcategory', function ($subQ) use ($searchValue) {
+                  $subQ->where('name', 'like', "%{$searchValue}%");
+              })
+              ->orWhereHas('mainCategory', function ($mainQ) use ($searchValue) {
+                  $mainQ->where('name', 'like', "%{$searchValue}%");
+              });
     }
+
+    $totalRecords = ChartOfAccount::count();
+    $filteredRecords = $query->count();
+
+    $chartAccounts = $query
+        ->offset($request->input('start'))
+        ->limit($request->input('length'))
+        ->get();
+
+    $data = $chartAccounts->map(function ($account) {
+        return [
+            'id'            => $account->id,
+            'account_name'  => $account->account_name,
+            'sub_category'  => $account->subcategory?->name ?? '-',
+            'main_category' => $account->mainCategory?->name ?? '-',
+        ];
+    });
+
+    return response()->json([
+        'draw' => intval($request->input('draw')),
+        'recordsTotal' => $totalRecords,
+        'recordsFiltered' => $filteredRecords,
+        'data' => $data
+    ]);
+}
+
 
     /**
      * Display Ledger details including cumulative credits/debits.
@@ -89,138 +75,7 @@ class LedgerController extends Controller
      * @param int $id
      * @return \Illuminate\View\View
      */
-    // public function details($id)
-    // {
-    //     $accountChart = ChartOfAccount::with([
-    //         'mainCategory',
-    //         'subcategory',
-    //         'journalEntries.journal.ledger',
-    //         'journalEntries.journal.creator'
-    //     ])->findOrFail($id);
-
-    //     $cumulativeCredit = 0;
-    //     $cumulativeDebit = 0;
-
-    //     $combinedData = collect([
-    //         (object)[
-    //             'type'                => 'chart',
-    //             'id'                  => $accountChart->id,
-    //             'created_at'          => $accountChart->created_at,
-    //             'entry_date'          => $accountChart->created_at,
-    //             'due_date'            => null,
-    //             'description'         => 'Account Chart',
-    //             'client_name'         => $accountChart->account_name,
-    //             'credit'              => 0,
-    //             'debit'               => 0,
-    //             'cumulative_credit'   => 0,
-    //             'cumulative_debit'    => 0,
-    //             'balance'             => 0,
-    //         ]
-    //     ]);
-
-    //     $ledgerData = $accountChart->journalEntries->map(function($entry) use (&$cumulativeCredit, &$cumulativeDebit) {
-    //         $journal = $entry->journal;
-    //         $ledger = $journal->ledger ?? null;
-
-    //         $credit = (float) $entry->credit_amount;
-    //         $debit  = (float) $entry->debit_amount;
-
-    //         $cumulativeCredit += $credit;
-    //         $cumulativeDebit  += $debit;
-
-    //         return (object)[
-    //             'type'                => 'entry',
-    //             'id'                  => $entry->id,
-    //             'created_at'          => $entry->created_at,
-    //             'entry_date'          => $journal->transaction_date ?? $entry->created_at,
-    //             'due_date'            => null,
-    //             'description'         => $entry->description,
-    //             'client_name'         => '-',
-    //             'credit'              => $credit,
-    //             'debit'               => $debit,
-    //             'cumulative_credit'   => $cumulativeCredit,
-    //             'cumulative_debit'    => $cumulativeDebit,
-    //             'balance'             => $cumulativeCredit - $cumulativeDebit,
-    //             'created_by'          => $journal->creator->name ?? '-'
-    //         ];
-    //     });
-
-    //     $combinedData = $combinedData->merge($ledgerData)->sortBy('created_at')->values();
-
-    //     return view('accounting::ledger.details', compact('accountChart', 'combinedData'));
-    // }
-
-
-//     public function details($id)
-// {
-//     $accountChart = ChartOfAccount::with([
-//         'mainCategory',
-//         'subcategory',
-//         'journalEntries.journal.ledger',
-//         'journalEntries.journal.creator'
-//     ])->findOrFail($id);
-
-//     // Fetch Opening Balance Data
-//     $openingBalance = OpeningBalance::where('chart_of_account_id', $id)->first();
-//     $openingCredit = $openingBalance->credit_amount ?? 0;
-//     $openingDebit = $openingBalance->debit_amount ?? 0;
-
-//     // Initialize cumulative totals with Opening Balance
-//     $cumulativeCredit = $openingCredit;
-//     $cumulativeDebit = $openingDebit;
-
-//     // First row: Account Chart with Opening Balance values
-//     $combinedData = collect([
-//         (object)[
-//             'type'                => 'chart',
-//             'id'                  => $accountChart->id,
-//             'created_at'          => $accountChart->created_at,
-//             'entry_date'          => $accountChart->created_at,
-//             'due_date'            => null,
-//             'description'         => 'Account Chart',
-//             'client_name'         => $accountChart->account_name,
-//             'credit'              => $openingCredit,
-//             'debit'               => $openingDebit,
-//             'cumulative_credit'   => $openingCredit,
-//             'cumulative_debit'    => $openingDebit,
-//             'balance'             => $openingCredit - $openingDebit,
-//         ]
-//     ]);
-
-//     // Loop Journal Entries and add them after the Chart row
-//     $ledgerData = $accountChart->journalEntries->map(function($entry) use (&$cumulativeCredit, &$cumulativeDebit) {
-//         $journal = $entry->journal;
-//         $ledger = $journal->ledger ?? null;
-
-//         $credit = (float) $entry->credit_amount;
-//         $debit  = (float) $entry->debit_amount;
-
-//         $cumulativeCredit += $credit;
-//         $cumulativeDebit  += $debit;
-
-//         return (object)[
-//             'type'                => 'entry',
-//             'id'                  => $entry->id,
-//             'created_at'          => $entry->created_at,
-//             'entry_date'          => $journal->transaction_date ?? $entry->created_at,
-//             'due_date'            => null,
-//             'description'         => $entry->description,
-//             'client_name'         => '-',
-//             'credit'              => $credit,
-//             'debit'               => $debit,
-//             'cumulative_credit'   => $cumulativeCredit,
-//             'cumulative_debit'    => $cumulativeDebit,
-//             'balance'             => $cumulativeCredit - $cumulativeDebit,
-//             'created_by'          => $journal->creator->name ?? '-'
-//         ];
-//     });
-
-//     // Merge Chart row + Ledger Entries
-//     $combinedData = $combinedData->merge($ledgerData)->sortBy('created_at')->values();
-
-//     return view('accounting::ledger.details', compact('accountChart', 'combinedData'));
-// }
-
+ 
 public function details($id)
 {
     $accountChart = ChartOfAccount::with([
